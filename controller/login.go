0300fdb6ae59ae50d810fe8520c166364c52cd79
thomas <thomasmaikon@hotmail.com>
2022-08-19 14:14:33 -0300

    ++

--- controller/login.go
+++ controller/login.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"thomas/projeto_mercafacil/models"
 	"thomas/projeto_mercafacil/service"
 
@@ -17,38 +16,19 @@ func Login(c *gin.Context) {
 	if err == nil {
 		c.JSON(200, gin.H{
 			"Token": service.CreateToken(user),
-			"User":  user,
 		})
 		return
 	}
-	fmt.Println(usuario)
+
 	dbConnection, err = factory.GetUserDB(models.TipoVarejao)
 	user, err = dbConnection.FindByUser(usuario)
 	if err == nil {
 		c.JSON(200, gin.H{
 			"Token": service.CreateToken(user),
-			"User":  user,
 		})
 		return
 	}
-	/* var dbUser models.Login
-	db.GetMysqlConnection().Where(&usuario).Find(&dbUser)
 
-	if strings.Compare(dbUser.Email, usuario.Email) == 0 {
-		c.JSON(200, gin.H{
-			"token": service.CreateToken(dbUser),
-		})
-		return
-	} */
-
-	/* db.GetPostgresConnection().Where(&usuario).Find(&dbUser)
-
-	if strings.Compare(dbUser.Email, usuario.Email) == 0 {
-		c.JSON(200, gin.H{
-			"token": service.CreateToken(dbUser),
-		})
-		return
-	} */
 	c.JSON(400, gin.H{
 		"Info":    "Usuario nao encontrado",
 		"Usuario": usuario.Email,
